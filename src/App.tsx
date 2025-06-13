ad0b11b894c37cfe1041ac97e3ef80c9513ac4b8
Bharath S <103253113+bharathsans@users.noreply.github.com>
2025-06-13 13:15:38 +0000

    Vite + React with TS setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-900">
+        Tailwind is Workings!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
